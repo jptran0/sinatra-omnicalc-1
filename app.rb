7dc0e8d67c23fb0ea785078ce46e06ba10516670
Jazmine Tran <117502378+jptran0@users.noreply.github.com>
2023-07-26 15:42:11 +0000

    square root pages done

--- app.rb
+++ app.rb
@@ -8,3 +8,22 @@ end
 get("/square/new") do
   erb(:square)
 end
+
+get("/square/results") do
+  @input = params.fetch("number")
+
+  @results = @input.to_f ** 2
+
+  erb(:square_results)
+end
+
+get("/square_root/new") do
+  erb(:root)
+end
+
+get("/square_root/results") do
+  @input = params.fetch("number")
+
+  @results = @input.to_f ** 0.5
+  erb(:root_results)
+end
