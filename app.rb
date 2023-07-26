b8086c4b933454b6cd34820c4e7ca7df36741574
Jazmine Tran <117502378+jptran0@users.noreply.github.com>
2023-07-26 16:22:19 +0000

    completed payment pages

--- app.rb
+++ app.rb
@@ -27,3 +27,25 @@ get("/square_root/results") do
   @results = @input.to_f ** 0.5
   erb(:root_results)
 end
+
+get("/payment/new") do
+  erb(:payment)
+end
+
+get("/payment/results") do
+  apr_input = params.fetch("apr").to_f.round(4)
+  @apr = apr_input.to_s(:percentage, { :precision => 4 })
+
+  @years = params.fetch("years").to_i
+  principal = params.fetch("principal").to_f
+  @pv = principal.to_s(:currency)
+
+  rate = ( apr_input / 12.0 ) / 100
+  n = -1 * (@years * 12)
+  numerator = rate * principal
+  denominator = 1 - (1 + rate ) ** n
+  # 27.1
+  @payment = (numerator / denominator).to_s(:currency)
+
+  erb(:payment_results)
+end
