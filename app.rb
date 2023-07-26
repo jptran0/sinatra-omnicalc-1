e180ac6c89f777d8a748f911e7b70f7b298ee6af
Jazmine Tran <117502378+jptran0@users.noreply.github.com>
2023-07-26 15:20:01 +0000

    created square page and form

--- app.rb
+++ app.rb
@@ -2,8 +2,9 @@ require "sinatra"
 require "sinatra/reloader"
 
 get("/") do
-  "
-  <h1>Welcome to your Sinatra App!</h1>
-  <p>Define some routes in app.rb</p>
-  "
+  erb(:square)
+end
+
+get("/square/new") do
+  erb(:square)
 end
